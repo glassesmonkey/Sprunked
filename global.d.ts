db848f99c965690def2eb0e77e4f1952612a91c3
glassesmonkey <530515490@qq.com>
2024-11-17 22:14:05 +0800

    fix bug

--- global.d.ts
+++
@@ -1,4 +0,0 @@
-interface Window {
-    gtag: (command: string, gaTrackingId: string, config: object) => void;
-    dataLayer: any[];
-  }
--- utils/environment.ts
+++
@@ -1,3 +0,0 @@
-export function isTestEnvironment(): boolean {
-    return process.env.NODE_ENV === 'test' || process.env.NEXT_PUBLIC_ENV === 'test';
-  }
